d8469210f1a673c650c664c034c339f577e8f441
Joel Brinson <jtbrinson11@gmail.com>
2022-07-21 10:10:10 -0400

    project updated

--- src/main/java/org/SpringAssignment1/App.java
+++ src/main/java/org/SpringAssignment1/App.java
@@ -1,13 +1,14 @@
 package org.SpringAssignment1;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("SpringAssignment1.xml");
+        student stud = (student) context.getBean("student");
+        System.out.println(stud);
     }
 }
